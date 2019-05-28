a0c6a90eeda035f10a149178dab46daa480e3d6e
Berthomé Hugo <hugo.berthome@epitech.eu>
2019-05-28 14:38:30 +0200

    [PROTO_ARCHI] adding PlayerController and KeyMap class

---
+++ includes/KeyMap.hpp
@@ -0,0 +1,46 @@
+/*
+** EPITECH PROJECT, 2018
+** Bomberman
+** File description:
+** KeyMap.hpp
+*/
+
+#ifndef KEY_MAP_HPP_
+#define KEY_MAP_HPP_
+
+#ifdef _WIN64
+#include <irrlicht.h>
+#else
+#include <irrlicht/irrlicht.h>
+#endif
+
+class KeyMap
+{
+private:
+    irr::EKEY_CODE _forward;
+    irr::EKEY_CODE _backward;
+    irr::EKEY_CODE _left;
+    irr::EKEY_CODE _right;
+    irr::EKEY_CODE _action;
+
+public:
+    KeyMap(irr::EKEY_CODE forward = irr::KEY_KEY_Z, irr::EKEY_CODE backward = irr::KEY_KEY_S, irr::EKEY_CODE left = irr::KEY_KEY_Q, irr::EKEY_CODE right = irr::KEY_KEY_D, irr::EKEY_CODE action = irr::KEY_SPACE);
+    ~KeyMap();
+
+    const irr::EKEY_CODE getForward() const;
+    void setForward(irr::EKEY_CODE);
+
+    const irr::EKEY_CODE getBackward() const;
+    void setBackward(irr::EKEY_CODE);
+
+    const irr::EKEY_CODE getLeft() const;
+    void setLeft(irr::EKEY_CODE);
+
+    const irr::EKEY_CODE getRight() const;
+    void setRight(irr::EKEY_CODE);
+
+    const irr::EKEY_CODE getAction() const;
+    void setAction(irr::EKEY_CODE);
+};
+
+#endif // !KEY_MAP_HPP_
--- includes/PlayerController.hpp
+++ includes/PlayerController.hpp
@@ -9,41 +9,26 @@
 #define PLAYER_CONTROLLER_HPP
 
 #include "GraphicalElements.hpp"
+#include "MyEventReceiver.hpp"
+#include "KeyMap.hpp"
 
 class PlayerController
 {
 private:
-    irr::SKeyMap _keyMap[5];
+    KeyMap _keyMap;
     GraphicalElements &_displayInfo;
 
 public:
     PlayerController(GraphicalElements &displayInfos);
-    PlayerController(irr::SKeyMap *_keyMap, GraphicalElements &displayInfos);
+    PlayerController(KeyMap keyMap, GraphicalElements &displayInfos);
     ~PlayerController();
+
+    const KeyMap &getKeyMap() const;
+    void setKeyMap(KeyMap &keyMap);
+
+    GraphicalElements &getDisplayInfo();
+
+    void action(MyEventReceiver &events);
 };
-
-PlayerController::PlayerController(GraphicalElements &displayInfos) : _displayInfo(displayInfos)
-{
-    _keyMap[0].Action = irr::EKA_MOVE_FORWARD;  // avancer
-    _keyMap[0].KeyCode = irr::KEY_KEY_Z;        // w
-    _keyMap[1].Action = irr::EKA_MOVE_BACKWARD; // reculer
-    _keyMap[1].KeyCode = irr::KEY_KEY_S;        // s
-    _keyMap[2].Action = irr::EKA_STRAFE_LEFT;   // a gauche
-    _keyMap[2].KeyCode = irr::KEY_KEY_Q;        // a
-    _keyMap[3].Action = irr::EKA_STRAFE_RIGHT;  // a droite
-    _keyMap[3].KeyCode = irr::KEY_KEY_D;        // d
-    _keyMap[4].Action = irr::EKA_JUMP_UP;       // saut
-    _keyMap[4].KeyCode = irr::KEY_SPACE;        // barre espace
-}
-
-PlayerController::PlayerController(irr::SKeyMap *keyMap, GraphicalElements &displayInfos) : _displayInfo(displayInfos)
-{
-    memcpy(_keyMap, keyMap, 5);
-}
-
-PlayerController::~PlayerController()
-{
-}
-
 
 #endif // !PLAYER_CONTROLLER_HPP
