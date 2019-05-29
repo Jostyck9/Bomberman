2139d89386ffd579c8e69f2f1c5382b59ca034f3
Berthomé Hugo <hugo.berthome@epitech.eu>
2019-05-29 10:25:41 +0200

    [PROTO_ARCHI] fixing compilation after modifying camera

--- includes/Camera.hpp
+++ includes/Camera.hpp
@@ -16,11 +16,14 @@ private:
     irr::scene::ISceneManager *_smgr;
     irr::core::vector3df _cameraPosition;
     irr::core::vector3df _cameraPointMire;
+    irr::scene::ICameraSceneNode *_camera;
 
 public:
-    Camera(irr::scene::ISceneManager *smgr, irr::core::vector3df _cameraPosition, irr::core::vector3df _cameraPointMire);
+    Camera(irr::scene::ISceneManager *smgr, irr::core::vector3df _cameraPosition = irr::core::vector3df(0, 0, 0), irr::core::vector3df _cameraPointMire = irr::core::vector3df(0, 0, 0));
     const irr::core::vector3df& getCameraPosition();
     const irr::core::vector3df& getCameraMirePoint();
+    void setCameraPosition(irr::core::vector3df &);
+    void setCameraMirePoint(irr::core::vector3df &);
 };
 
 #endif
