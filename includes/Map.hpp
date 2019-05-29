0980032d2ca019c7b81f66ecaf987d49c81f5d04
jules casimiro <jules.casimiro@epitech.eu>
2019-05-29 10:56:55 +0200

    [Map] Add MapGen in Map class

--- includes/Map.hpp
+++ includes/Map.hpp
@@ -21,14 +21,16 @@ class Map
 {
 private:
     boost::multi_array<std::vector<GameObject*>, 2> _map;
+    std::vector<std::string> _mapGen;
     irr::u16 _width;
     irr::u16 _heigh;
 
 public:
-    Map(irr::u16 width, irr::u16 heigh);
+    Map(irr::u16 size);
     ~Map();
 
-    void setMap(std::vector<std::string> map);
+    void genMap(irr::u16 size);
+    void setMap();
     void addToMap(irr::u16 x, irr::u16 y, GameObject *obj);
 };
 
--- src/Map.cpp
+++ src/Map.cpp
@@ -8,24 +8,71 @@
 #include "Map.hpp"
 #include "Wall.hpp"
 
-Map::Map(irr::u16 width, irr::u16 heigh) : _map(boost::extents[width][heigh]), _width(width), _heigh(heigh)
+Map::Map(irr::u16 size) : _map(boost::extents[size][size]), _width(size), _heigh(size)
 {
+    genMap(size);
 }
 
 Map::~Map()
 {
 }
 
-void Map::setMap(std::vector<std::string> map)
+void Map::genMap(irr::u16 size)
 {
-    for (size_t i = 0; i < map.size(); i++) {
-        for (size_t j = 0; j < map.size(); j++) {
-            if (map.at(i).at(j) == 'X') {
-                Wall newWall(false);
+    std::string str;
+
+    if (size % 2 == 0)
+        size++;
+    for (int i = 0; i < size; i++)
+        str += '_';
+    for (int i = 0; i < size; i++)
+        _mapGen.push_back(str);
+/*    for (int i = 0; i < (size * size) / 4 * 3; i++)
+        _mapGen.at(std::rand() % size).at(std::rand() % size) = 'X';
+    for (int i = 0; i < (size * size) / 3 * 2; i++)
+        _mapGen.at(std::rand() % size).at(std::rand() % size) = 'O';
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - 1; j++) {
+            if (_mapGen.at(i).at(j) != 'X' && _mapGen.at(i + 1).at(j) != 'X' &&
+                _mapGen.at(i).at(j + 1) != 'X' && _mapGen.at(i + 1).at(j + 1) != 'X')
+                _mapGen.at(i + rand() % 2).at(j + rand() % 2) = 'X';
+        }
+    }*/
+    for (irr::u16 i = 0; i < (size * size) - size; i++)
+        _mapGen.at(std::rand() % size).at(std::rand() % size) = 'O';
+    for (irr::u16 i = 1; i < size; i = i + 2) {
+        for (irr::u16 j = 1; j < size; j = j + 2) {
+            _mapGen.at(i).at(j) = 'X';
+        }
+    }
+    _mapGen.at(0).at(0) = '_';
+    _mapGen.at(1).at(0) = '_';
+    _mapGen.at(0).at(1) = '_';
+    _mapGen.at(1).at(1) = 'X';
+    _mapGen.at(size - 1).at(0) = '_';
+    _mapGen.at(size - 1).at(1) = '_';
+    _mapGen.at(size - 2).at(0) = '_';
+    _mapGen.at(size - 2).at(1) = 'X';
+    _mapGen.at(0).at(size - 1) = '_';
+    _mapGen.at(1).at(size - 1) = '_';
+    _mapGen.at(0).at(size - 2) = '_';
+    _mapGen.at(1).at(size - 2) = 'X';
+    _mapGen.at(size - 1).at(size - 1) = '_';
+    _mapGen.at(size - 2).at(size - 1) = '_';
+    _mapGen.at(size - 1).at(size - 2) = '_';
+    _mapGen.at(size - 2).at(size - 2) = 'X';
+}
+
+void Map::setMap()
+{
+    for (irr::u16 i = 0; i < _mapGen.size(); i++) {
+        for (irr::u16 j = 0; j < _mapGen.size(); j++) {
+            if (_mapGen.at(i).at(j) == 'X') {
+                Wall newWall(false, i, j);
                 addToMap(i, j, dynamic_cast<GameObject*>(&newWall));
             }
-            if (map.at(i).at(j) == 'o') {
-                Wall newWall(true);
+            if (_mapGen.at(i).at(j) == 'o') {
+                Wall newWall(true, i, j);
                 addToMap(i, j, dynamic_cast<GameObject*>(&newWall));
             }
         }
