06e4794fba16c35a235692494adb0d874b43c929
Berthomé Hugo <hugo.berthome@epitech.eu>
2019-06-14 12:42:32 +0200

    adding wallPass

--- src/GameObject/Character/Player.cpp
+++ src/GameObject/Character/Player.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include "AItem.hpp"
 #include "Player.hpp"
+#include "Wall.hpp"
 
 Player::Player(irr::IrrlichtDevice* device, std::vector<std::string> path_text, std::string &path_mesh, irr::s16 pos_x, irr::s16 pos_y) : ACharacter(device)
 {
@@ -26,8 +27,8 @@ void Player::update(Map &map, std::vector<irr::s32> &idToDel, MyEventReceiver ev
 {
     irr::scene::ISceneNode *node = NULL;
     std::shared_ptr<GameObject> obj = nullptr;
+    irr::core::vector3df newPos;
 
-    this->getPlayerController().action(_device, event, map, getStats().getSpeed());
     // irr::core::vector3df pos = this->getDisplayInfo().getPosition();
     // irr::u16 valx = dynamic_cast<irr::u16>(pos.X) / 10;
     // irr::u16 valy = dynamic_cast<irr::u16>(pos.Y) / 10;
@@ -35,11 +36,26 @@ void Player::update(Map &map, std::vector<irr::s32> &idToDel, MyEventReceiver ev
     node = getDisplayInfo().getFrontObj(4, GameObject::ITEM);
     if (node != nullptr) {
         obj = map.getObject(node);
-        if (!obj)
-            return;
-        std::dynamic_pointer_cast<AItem>(obj)->applyEffect(*this);
-        idToDel.push_back(obj->getID());
+        if (obj) {
+            std::dynamic_pointer_cast<AItem>(obj)->applyEffect(*this);
+            idToDel.push_back(obj->getID());
+        }
     }
+    if (getStats().getPassThrough()) {
+        node = getDisplayInfo().getFrontObj(2, GameObject::WALL);
+        if (node) {
+            obj = map.getObject(node);
+            if (obj) {
+                newPos = std::dynamic_pointer_cast<Wall>(obj)->getDisplayInfo().getPosition();
+                newPos.Y += 3;
+                newPos.Z = -20;
+                getDisplayInfo().setPosition(newPos);
+                getStats().setPassThrough(false);
+                std::cout << "WALLPASS" << std::endl;
+            }
+        }
+    }
+    this->getPlayerController().action(_device, event, map, getStats().getSpeed());
 }
 
 GameObject::objecType_t Player::getType()
